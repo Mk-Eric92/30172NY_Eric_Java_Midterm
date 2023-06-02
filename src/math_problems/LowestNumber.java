32ba844fa41ab52b8a1aac705c0cb0f04989c112
Mk-Eric92 <ericmuhaimin6192@gmail.com>
2023-06-01 23:55:04 -0400

    Solved FindMissingNumber

--- src/math_problems/LowestNumber.java
+++ src/math_problems/LowestNumber.java
@@ -11,7 +11,19 @@ public class LowestNumber {
         int[] array = new int[] {211, 110, 99, 34, 67, 89, 67, 456, 321, 456, 78, 90, 45, 32, 56, 78, 90, 54, 32, 123,
                 67, 5, 679, 54, 32, 65};
 
+        int lowestNumber = findLowestNumber(array);
+        System.out.println("Lowest number from the array: " + lowestNumber);
+    }
 
+    public static int findLowestNumber(int[] array) {
+        int lowest = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < lowest) {
+                lowest = array[i];
+            }
+        }
+        return lowest;
     }
 
 }
