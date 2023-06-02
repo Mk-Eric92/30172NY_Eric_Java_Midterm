18e96641d84feae56593fe81404d367c9ed449f6
Mk-Eric92 <ericmuhaimin6192@gmail.com>
2023-06-01 20:45:46 -0400

    Sloved math_problem-Fibonacci

--- src/math_problems/Fibonacci.java
+++ src/math_problems/Fibonacci.java
@@ -9,6 +9,24 @@ public class Fibonacci {
      **/
 
     public static void main(String[] args) {
+        int count = 40;
 
+        printFibonacciNumbers(count);
+    }
+
+    public static void printFibonacciNumbers(int count) {
+        long[] fibonacci = new long[count];
+        fibonacci[0] = 0;
+        fibonacci[1] = 1;
+
+        System.out.println("Fibonacci Numbers:");
+
+        for (int i = 2; i < count; i++) {
+            fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+        }
+
+        for (int i = 0; i < count; i++) {
+            System.out.print(fibonacci[i] + " ");
+        }
     }
 }
