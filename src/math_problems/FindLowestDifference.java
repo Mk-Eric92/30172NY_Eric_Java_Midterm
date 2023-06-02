1783782a2e6fdb663643b50a4e973e0c7f29e077
Mk-Eric92 <ericmuhaimin6192@gmail.com>
2023-06-01 21:04:30 -0400

    Solved FindLowestDifference

--- src/math_problems/FindLowestDifference.java
+++ src/math_problems/FindLowestDifference.java
@@ -1,5 +1,7 @@
 package math_problems;
 
+import java.util.HashSet;
+
 public class FindLowestDifference {
 
     /** INSTRUCTIONS
@@ -13,6 +15,26 @@ public class FindLowestDifference {
         int[] array1 = {30, 12, 5, 9, 2, 20, 33, 1, -15};
         int[] array2 = {18, 25, 41, 47, 17, 36, 14, 19, -15};
 
+        int lowestDifference = findLowestDifference(array1, array2);
+        System.out.println("Lowest number not shared between the arrays: " + lowestDifference);
+    }
+
+    public static int findLowestDifference(int[] array1, int[] array2) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : array1) {
+            set.add(num);
+        }
+
+        int lowestDifference = Integer.MAX_VALUE;
+
+        for (int num : array2) {
+            if (!set.contains(num) && num < lowestDifference) {
+                lowestDifference = num;
+            }
+        }
+
+        return lowestDifference;
     }
 
 
