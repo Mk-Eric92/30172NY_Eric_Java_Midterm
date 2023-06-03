9d60476a01871b335dc231d8802c2a0baf2d5dd3
Mk-Eric92 <ericmuhaimin6192@gmail.com>
2023-06-02 20:51:16 -0400

    Solved PatternNumber

--- src/math_problems/PrimeNumber.java
+++ src/math_problems/PrimeNumber.java
@@ -9,9 +9,23 @@ public class PrimeNumber {
      * BONUS: Figure out how to improve algorithmic efficiency
      */
 
-
+    public static boolean primeNumber(int p) {
+        for (int i = 2; i <= p/i; i++) {
+            if (p % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
     public static void main(String[] args) {
 
+        System.out.println("Prime number from 2 to 100000 are: ");
+        int minimum = 2, maximum = 100000;
+        for (int p = minimum; p <+ maximum; p++) {
+            if (primeNumber(p)) {
+                System.out.print(p + " ");
+            }
+        }
     }
 
 }
