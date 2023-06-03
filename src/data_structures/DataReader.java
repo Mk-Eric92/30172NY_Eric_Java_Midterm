84335158759ce0d11027eda21fcb200e039cc99f
Mk-Eric92 <ericmuhaimin6192@gmail.com>
2023-06-02 21:22:01 -0400

    Completed DataReader

--- src/data_structures/DataReader.java
+++ src/data_structures/DataReader.java
@@ -1,5 +1,13 @@
 package data_structures;
 
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Stack;
+
 public class DataReader {
 
     /** INSTRUCTIONS
@@ -21,6 +29,65 @@ public class DataReader {
     public static void main(String[] args) {
         String textFilePath = System.getProperty("user.dir") + "/src/data_structures/data/self-driving-car.txt";
 
+        try {
+            readFileAndPrint(textFilePath);
+
+            LinkedList<String> linkedList = storeWordsInLinkedList(textFilePath);
+            System.out.println("Linked List:");
+
+
+            for (String word : linkedList) {
+                System.out.println(word);
+            }
+            Stack<String> stack = storeWordsInStack(textFilePath);
+            System.out.println("Stack:");
+
+            Iterator<String> iterator = stack.iterator();
+            while (iterator.hasNext()) {
+                System.out.println(iterator.next());
+            }
+
+            System.out.println("Stack Operations:");
+            stack.push("New Item");
+            System.out.println("Peek: " + stack.peek());
+            System.out.println("Search 'Stack': " + stack.search("Stack"));
+            System.out.println("Pop: " + stack.pop());
+        } catch (Exception e) {
+            System.out.println("Error occurred: " + e.getMessage());
+        }
     }
+    public static void readFileAndPrint(String filePath) throws Exception {
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        String line;
+        System.out.println("File Contents:");
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
+        reader.close();
+    }
+    public static LinkedList<String> storeWordsInLinkedList(String filePath) throws Exception {
+        LinkedList<String> linkedList = new LinkedList<>();
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] words = line.split("\\s+");
+            linkedList.addAll(Arrays.asList(words));
+        }
+        reader.close();
+        return linkedList;
+    }
+    public static Stack<String> storeWordsInStack(String filePath) throws Exception {
+        Stack<String> stack = new Stack<>();
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] words = line.split("\\s+");
+            for (String word : words) {
+                stack.push(word);
+            }
+        }
+        reader.close();
+        return stack;
+}
 
 }
