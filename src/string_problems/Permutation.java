eca4ef989c84ea3fc979524660165f0678367e38
Mk-Eric92 <ericmuhaimin6192@gmail.com>
2023-06-03 22:17:49 -0400

    Completed string_problems - Permutation

--- src/string_problems/Permutation.java
+++ src/string_problems/Permutation.java
@@ -10,6 +10,31 @@ public class Permutation {
 
     public static void main(String[] args) {
 
+        String word = "GAME";
+        computePermutations(word);
     }
     // Implement Here
+    public static void computePermutations(String word) {
+        permute(word, 0, word.length() - 1);
+    }
+
+    public static void permute(String word, int left, int right) {
+        if (left == right) {
+            System.out.println(word);
+        } else {
+            for (int i = left; i <= right; i++) {
+                word = swap(word, left, i);
+                permute(word, left + 1, right);
+                word = swap(word, left, i);
+            }
+        }
+    }
+
+    public static String swap(String word, int i, int j) {
+        char[] chars = word.toCharArray();
+        char temp = chars[i];
+        chars[i] = chars[j];
+        chars[j] = temp;
+        return String.valueOf(chars);
+    }
 }
