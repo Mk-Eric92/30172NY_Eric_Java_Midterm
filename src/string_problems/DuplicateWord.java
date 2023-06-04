2f788f9fb6ce02fb501494acafcebe54ea6a5282
Mk-Eric92 <ericmuhaimin6192@gmail.com>
2023-06-03 22:01:00 -0400

    Completed string_problems - DuplicateWord

--- src/string_problems/DuplicateWord.java
+++ src/string_problems/DuplicateWord.java
@@ -1,5 +1,8 @@
 package string_problems;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DuplicateWord {
 
     /** INSTRUCTIONS
@@ -12,6 +15,38 @@ public class DuplicateWord {
 
         // Implement here
 
+        Map<String, Integer> wordCount = findDuplicateWords(st);
+        double averageLength = calculateAverageLength(st);
+
+        System.out.println("Duplicate Words:");
+        for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
+            String word = entry.getKey();
+            int count = entry.getValue();
+            System.out.println(word + ": " + count);
+        }
+        System.out.println("Average Word Length: " + averageLength);
+    }
+
+    public static Map<String, Integer> findDuplicateWords(String sentence) {
+        Map<String, Integer> wordCount = new HashMap<>();
+        String[] words = sentence.toLowerCase().split("\\W+");
+
+        for (String word : words) {
+            wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+        }
+
+        return wordCount;
+    }
+
+    public static double calculateAverageLength(String sentence) {
+        String[] words = sentence.split("\\W+");
+        int totalLength = 0;
+
+        for (String word : words) {
+            totalLength += word.length();
+        }
+
+        return (double) totalLength / words.length;
     }
 
 }
