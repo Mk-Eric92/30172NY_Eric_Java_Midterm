79f08152ae29655193d106cf5419633e1200020b
Mk-Eric92 <ericmuhaimin6192@gmail.com>
2023-06-03 22:35:45 -0400

    Completed data_structures - UseArrayList

--- src/data_structures/UseArrayList.java
+++ src/data_structures/UseArrayList.java
@@ -1,5 +1,7 @@
 package data_structures;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 public class UseArrayList {
 
     /** INSTRUCTIONS
@@ -11,6 +13,41 @@ public class UseArrayList {
      */
 
     public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Orange");
+
+        String firstElement = list.get(0);
+        System.out.println("First Element: " + firstElement);
+
+        list.remove(1);
+
+
+        System.out.println("Data using for-each loop:");
+        for (String element : list) {
+            System.out.println(element);
+        }
+
+        System.out.println("Data using while-loop with iterator:");
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            String element = iterator.next();
+            System.out.println(element);
+        }
+
+        System.out.println("Storing and retrieving data from a database table:");
+
+        for (String element : list) {
+
+            System.out.println("Inserting into database: " + element);
+        }
+
+        for (String element : list) {
+            System.out.println("Retrieved from database: " + element);
+        }
 
     }
 
